921ed7f8c39daf3e6ba26424f6cf54b4db1f3070
lyfnsmile <li.zhixiang@live.cn>
2016-07-05 14:59:50 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+```
+npm install
+```
+# then
+
+```
+npm start
+```
+
+# open ``http://localhost:3000/`` in browser.
---
+++ app/actions/index.js
@@ -0,0 +1,42 @@
+'use strict';
+//action type
+export const ADD_TODO = 'ADD_TODO';
+export const COMPLETE_TODO = 'COMPLETE_TODO';
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
+
+export const VisibilityFilters = {
+    SHOW_ALL: 'SHOW_ALL',
+    SHOW_COMPLETED: 'SHOW_COMPLETED',
+    SHOW_ACTIVE: 'SHOW_ACTIVE',
+};
+
+export function addTodo(text) {
+    return {
+        type: ADD_TODO,
+        text
+    };
+}
+
+export function completeTodo(index) {
+    return {
+        type: COMPLETE_TODO,
+        index
+    };
+}
+
+export function setVisibilityFilter(filter) {
+    console.log(filter)
+    return {
+        type: SET_VISIBILITY_FILTER,
+        filter
+    };
+}
+
+//初始化
+export function fetchPosts(filter) {
+    return dispatch => {
+        return fetch('http://localhost:4000/db')
+            .then(response => response.json())
+            .then(json => dispatch(setVisibilityFilter(json)))
+    }
+}
